84eca906612def6c49247581996bcb012ea84a3b
moulina pradhan <pmoulina76@gmail.com>
2021-04-25 20:31:31 +0530

    deploy on heroku

--- src/Components/ContactMeForm.js
+++ src/Components/ContactMeForm.js
@@ -11,6 +11,8 @@ import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+
 
 const defaultTheme = createMuiTheme({})
   const { breakpoints, typography: { pxToRem } } = defaultTheme
@@ -98,6 +100,11 @@ const DialogActions = withStyles((theme) => ({
 export default function ContactMeForm() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
+  const { transcript, resetTranscript } = useSpeechRecognition()
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null
+  }
   
   const handleClickOpen = () => {
     setOpen(true);
